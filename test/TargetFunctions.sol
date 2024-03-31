e1d1d7d85ba78f98a50c9feb07b3f8532feef545
Eugenio <eugenioclrc@gmail.com>
2024-03-31 09:58:21 -0300

    this invariants should always fail

--- test/TargetFunctions.sol
+++ test/TargetFunctions.sol
@@ -7,25 +7,38 @@ import {Properties} from "./Properties.sol";
 import {vm} from "@chimera/Hevm.sol";
 
 abstract contract TargetFunctions is BaseTargetFunctions, Properties, BeforeAfter {
-
-    function iWETH9_approve(address guy, uint256 wad) public {
-      iWETH9.approve(guy, wad);
+    modifier _updateBeforeAfter() {
+        __before();
+        _;
+        __after();
     }
 
-    function iWETH9_deposit(uint256 amount) public {
-      iWETH9.deposit{value: amount}();
+    function assertTotalSupply() public {
+        uint256 b = _before.iWETH9_totalSupply;
+        uint256 a = _after.iWETH9_totalSupply;
+
+        /// the follow invariants shouyld be triggered
+        eq(iWETH9.totalSupply(), b, "totalSupply broken");
+        eq(a, b, "totalSupply broken");
+    }
+
+    function iWETH9_deposit() public _updateBeforeAfter {
+        iWETH9.deposit{value: 1}();
+    }
+
+    function iWETH9_approve(address guy, uint256 wad) public {
+        iWETH9.approve(guy, wad);
     }
 
     function iWETH9_transfer(address dst, uint256 wad) public {
-      iWETH9.transfer(dst, wad);
+        iWETH9.transfer(dst, wad);
     }
 
     function iWETH9_transferFrom(address src, address dst, uint256 wad) public {
-      iWETH9.transferFrom(src, dst, wad);
+        iWETH9.transferFrom(src, dst, wad);
     }
 
     function iWETH9_withdraw(uint256 wad) public {
-      iWETH9.withdraw(wad);
+        iWETH9.withdraw(wad);
     }
 }
-
